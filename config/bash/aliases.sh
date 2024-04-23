c785b8f5603baa5e462aff4c3876d7e62a047e29
Dlnya Dlzar <dlnyadlzar@gmail.com>
2024-04-23 10:53:03 +0300

    refactoring

---
+++ config/bash/aliases.sh
@@ -0,0 +1,15 @@
+# bash aliases
+
+
+# exa
+alias ls='exa --icons --group-directories-first'
+alias la='exa --icons --all --group-directories-first'
+alias ll='exa --long --icons --group-directories-first'
+alias lla='exa --long --all --icons --group-directories-first'
+alias lt='exa --tree --level=2 --icons'
+alias lta='exa --tree --all --level=2 --icons'
+alias llt='exa --long --tree --level=2 --icons'
+alias llta='exa --long --tree --all --level=2 --icons'
+
+# batcat
+alias cat='batcat'
